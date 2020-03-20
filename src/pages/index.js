99d447a18191392dfbd94e97adfaee67e7b797b4
Ruangwit Sanguanpong <ruangwit.s@TorThuys-MacBook-Pro.local>
2020-03-20 14:42:26 +0700

    intial project

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { Button } from 'antd'
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <p>hello</p>
+    <Button>TEST</Button>
+  </div>
+)
+
+export default Home
